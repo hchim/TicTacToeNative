187241c215b4782ad3ee6fb7035ef2b70e404e48
Hui Chen <huiche@amazon.com>
2018-04-03 02:29:38 -0700

    Use react-navigation-header-buttons to implement header button.

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { StyleSheet, View, Button } from 'react-native';
 import GameBoard from '../containers/GameBoard';
 import GameStatus from '../containers/GameStatus';
-import HeaderButton from './HeaderButton';
+import HeaderButtons from 'react-navigation-header-buttons'
+import { Ionicons } from '@expo/vector-icons';
 
 class HomeScreen extends React.Component {
 
     static navigationOptions = ({navigation}) => {
+        const params = navigation.state.params || {};
         return {
             title: 'Home',
             headerRight: (
-                <HeaderButton
-                    title="Settings"
-                    screen="Settings"
-                    navigation={navigation}
-                    icon={require('../images/ic_settings_white_48dp.png')} />
+                <HeaderButtons IconComponent={Ionicons} iconSize={23} color="#fff">
+                    <HeaderButtons.Item
+                        title="Settings"
+                        iconName="ios-settings"
+                        onPress={() => params.onPress('Settings')} />
+                </HeaderButtons>
             )
         }
     };
 
-    //Set parameters to navigation is needed
-    // componentWillMount() {
-    //     this.props.navigation.setParams({});
-    // }
+    _navigateTo = (screen) => {
+        this.props.navigation.navigate(screen);
+    }
+
+    componentWillMount() {
+        this.props.navigation.setParams({
+            onPress: this._navigateTo
+        });
+    }
 
     render() {
         return (
